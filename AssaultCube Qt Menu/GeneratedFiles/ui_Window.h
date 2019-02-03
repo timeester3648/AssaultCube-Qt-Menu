f8861c91e0cbd128ffc7d8bd502bcca6c18dd942
timeester3648 <weteringt@gmail.com>
2019-02-03 16:26:39 +0100

    Updated to Qt 5.13.0

--- AssaultCube Qt Menu/GeneratedFiles/ui_Window.h
+++ AssaultCube Qt Menu/GeneratedFiles/ui_Window.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'Window.ui'
 **
-** Created by: Qt User Interface Compiler version 5.12.0
+** Created by: Qt User Interface Compiler version 5.13.0
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
@@ -37,7 +37,7 @@ public:
 
     void retranslateUi(QMainWindow *WindowClass)
     {
-        WindowClass->setWindowTitle(QApplication::translate("WindowClass", "Window", nullptr));
+        WindowClass->setWindowTitle(QCoreApplication::translate("WindowClass", "Window", nullptr));
     } // retranslateUi
 
 };
